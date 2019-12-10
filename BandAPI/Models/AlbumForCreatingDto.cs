dd0a8831d8077e144c7ec97c420169647b36f383
Pavol <pavol@homerundesign.com>
2019-12-09 21:52:50 -0500

    Adding Validation

--- BandAPI/Models/AlbumForCreatingDto.cs
+++ BandAPI/Models/AlbumForCreatingDto.cs
@@ -1,13 +1,28 @@
-﻿using System;
+﻿using BandAPI.ValidationAttributes;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BandAPI.Models
 {
-    public class AlbumForCreatingDto
+    [TitleAndDescription(ErrorMessage = "Title Must Be Different From Description")]
+    public class AlbumForCreatingDto //: IValidatableObject
     {
+        [Required(ErrorMessage = "Title needs to be filled in")]
+        [MaxLength(200, ErrorMessage ="Title needs to be up to 200 characters")]
         public string Title { get; set; }
+
+        [MaxLength(400, ErrorMessage = "Title needs to be up to 400 characters")]
         public string Description { get; set; }
+
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    if (Title == Description)
+        //    {
+        //        yield return new ValidationResult("The title and description need to be different", new[] { "AlbumForCreatingDto" });
+        //    }
+        //}
     }
 }
---
+++ BandAPI/ValidationAttributes/TitleAndDescriptionAttribute.cs
@@ -0,0 +1,24 @@
+﻿using BandAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BandAPI.ValidationAttributes
+{
+    public class TitleAndDescriptionAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var album = (AlbumForCreatingDto)validationContext.ObjectInstance;
+
+            if (album.Title == album.Description)
+            {
+                return new ValidationResult("The title and the description need to be different", new[] { "AlbumForCreatingDto" });
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
